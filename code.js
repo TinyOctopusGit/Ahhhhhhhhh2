3ae438ae730d2771cac755bd73d669c1717733a0
TinyOctopusGit <95771894+TinyOctopusGit@users.noreply.github.com>
2023-04-18 13:45:14 -0400

    Update code.js

--- code.js
+++ code.js
@@ -4,7 +4,13 @@ document.getElementById("frm1").onsubmit = (e) => {
     const APIKey = document.getElementById("APIKey").value;
     const gYear = document.getElementById("gYear").value;
     const output = document.getElementById("result");
-    const teamID = document.getElementById("tNumber").value;
+    const teamID = "frc"+document.getElementById("tNumber").value;
+    const showWin = document.getElementById("win").checked;
+    const showLose = document.getElementById("lose").checked;
+    const showTie = document.getElementById("tie").checked;
+
+    document.getElementById("result").innerHTML = "";
+
     fetch(`${baseURL}/team/${teamID}/matches/${gYear}`,{
         method: "GET",
         headers: {
@@ -13,11 +19,61 @@ document.getElementById("frm1").onsubmit = (e) => {
     }).then(k=> {
         k.json().then(p=>{
             output.value = JSON.stringify(p.map(f=>{
-                const isBlue = f['alliances']['blue']['team_keys'].find(b=>b===teamID);
-                return {
-                    eventKey: f['event_key'],
-                    teamName: isBlue ? "blue" : "red",
-                    score: isBlue ? f['alliances']['blue']['score'] : f['alliances']['red']['score'],
+
+                const url = ("https://www.thebluealliance.com/match/"+f['key']);
+                const match_number = f['match_number']
+                const allianceWinner = f['winning_alliance'];
+                var allianceLoser = "red";
+                const comp_level = f['comp_level'];
+                var compType = "Match";
+
+                //Find Alliance Loser
+                if (allianceWinner == "red") {
+                    allianceLoser = "blue";
+                } else if(allianceWinner == "blue") {
+                    allianceLoser = "red";
+                } else {
+                    allianceLoser = "Tie";
+                }
+
+                //Get Match Type
+                if (comp_level == "f") {
+                    compType = "Final";
+                }
+                else if (comp_level == "qm") {
+                    compType = "Qualifying";
+                }
+                else if (comp_level == "sf") {
+                    compType = "SemiFinal";
+                }
+
+                try {
+                    const score = f['alliances'][allianceWinner]['score'] + "-" +f['alliances'][allianceLoser]['score'];
+
+                    if (showWin) {
+                        //If TeamID is found in allianceWinner path then:
+                        if (Boolean(f['alliances'][allianceWinner]['team_keys'].find(b=>b===teamID))) {
+                            document.getElementById("result").innerHTML += 
+                                "<a href="+url+" target='_blank'>Win: "+compType+" "+match_number+": "+allianceWinner+" wins! "+score+"</a>" + "<br>";
+                            console.log("Win");
+                        }
+                    }
+
+                    if (showLose) {
+                        //If TeamID is found in allianceLoser path then:
+                        if(Boolean(f['alliances'][allianceLoser]['team_keys'].find(b=>b===teamID))) {
+                            document.getElementById("result").innerHTML += 
+                                "<a href="+url+" target='_blank'>Loss: "+compType+" "+match_number+": "+allianceWinner+" wins! "+score+"</a>" + "<br>";
+                        }
+                    }
+
+                } catch (error) {
+
+                    if (showTie) {
+                        score = f['alliances']['blue']['score'] + "-" +f['alliances']['red']['score']
+                        document.getElementById("result").innerHTML += 
+                        "<a href="+url+" target='_blank'>Tie: "+compType+" "+match_number+": "+""+score+"</a>" + "<br>";
+                    }
                 }
             }));
         })
